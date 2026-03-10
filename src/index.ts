9136f7f074af416ae8727f64c1b0285f90123bdc
Sumanth Bojugu <bojugusumanthkumar@gmail.com>
2026-03-11 00:26:46 +0530

    chore: create express app

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { app } from "./api/app.js";
+
+app.listen(8080, () => {
+  console.log("====================================");
+  console.log("listening on port 8080");
+  console.log("====================================");
+});
